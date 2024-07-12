29b3759388c2f6a02b86dc82a3142e59e6fb0830
Tristan Leduc <61935382+tristanleduc@users.noreply.github.com>
2024-07-12 11:53:40 +0200

    modified paths

--- src/training.py
+++ src/training.py
@@ -26,7 +26,7 @@ mask_train_dir_cityscapes = os.path.join(os.getcwd(), 'dataset', 'gtFine', 'trai
 image_val_dir_cityscapes = os.path.join(os.getcwd(), 'dataset', 'leftImg8bit', 'val')
 mask_val_dir_cityscapes = os.path.join(os.getcwd(), 'dataset', 'gtFine', 'val')
 
-image_dir_ade20K = os.path.join(os.getcwd(), 'dataset', 'ADE20K_2021_17_01', 'images', 'ADE')
+image_dir_ade20K = os.path.join(os.getcwd(), '..', 'dataset', 'ADE20K_2021_17_01', 'images', 'ADE')
 
 train_dataset_cityscapes = CityscapesSkyDataset(image_dir=image_train_dir_cityscapes, mask_dir=mask_train_dir_cityscapes, transform=transform, target_transform=target_transform)
 val_dataset_cityscapes = CityscapesSkyDataset(image_dir=image_val_dir_cityscapes, mask_dir=mask_val_dir_cityscapes, transform=transform, target_transform=target_transform)
@@ -34,22 +34,20 @@ val_dataset_cityscapes = CityscapesSkyDataset(image_dir=image_val_dir_cityscapes
 train_dataset_ade20k = ADE20KSkyDataset(image_dir=image_dir_ade20K, index_file='index_ade20k.pkl', transform=transform, target_transform=target_transform, split='train')
 val_dataset_ade20k = ADE20KSkyDataset(image_dir=image_dir_ade20K, index_file='index_ade20k.pkl', transform=transform, target_transform=target_transform, split='val')
 
-random_indices = random.sample(range(0, len(train_dataset_ade20k)), 3000) 
+random_indices = random.sample(range(0, len(train_dataset_ade20k)), 4000) 
 train_dataset_ade20k = torch.utils.data.Subset(train_dataset_ade20k, random_indices)
 
 # concatenating the two datasets 
 train_dataset = train_dataset_ade20k
 
-train_loader = DataLoader(train_dataset, batch_size=15, shuffle=True)
-val_loader_cityscapes = DataLoader(val_dataset_cityscapes, batch_size=15, shuffle=False)
-val_loader_ade20k = DataLoader(val_dataset_ade20k, batch_size=15, shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=13, shuffle=True)
+val_loader_cityscapes = DataLoader(val_dataset_cityscapes, batch_size=17, shuffle=False)
+val_loader_ade20k = DataLoader(val_dataset_ade20k, batch_size=13, shuffle=False)
 
 # loading the model weights saved in pth 
 model = models.segmentation.deeplabv3_resnet101(weights=DeepLabV3_ResNet101_Weights.COCO_WITH_VOC_LABELS_V1)
 model.classifier[4] = torch.nn.Conv2d(256, 1, kernel_size=1)
-model.load_state_dict(torch.load('../models/sky_segmentation_model_15_epochs.pth'))
-#model = models.segmentation.deeplabv3_resnet101(weights=DeepLabV3_ResNet101_Weights.COCO_WITH_VOC_LABELS_V1)
-#model.classifier[4] = torch.nn.Conv2d(256, 1, kernel_size=1)
+#model.load_state_dict(torch.load('../models/sky_segmentation_model_15_epochs.pth'))
 
 if torch.cuda.is_available():
     device = torch.device('cuda')
@@ -62,7 +60,7 @@ print(f'Using device {device}')
 model = model.to(device)
 
 # Training parameters
-num_epochs = 10
+num_epochs = 30
 initial_lr = 0.0008
 
 optimizer = torch.optim.Adam(model.parameters(), lr=initial_lr)
@@ -73,7 +71,18 @@ writer = SummaryWriter(log_dir='runs/sky_segmentation_6')
 criterion = torch.nn.BCEWithLogitsLoss()
 criterion.to(device)
 
+def save_checkpoint(epoch, model, optimizer, loss, filename="checkpoint.pth.tar"):
+    state = {
+        'epoch': epoch,
+        'state_dict': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+        'loss': loss
+    }
+    torch.save(state, filename)
+
 if __name__ == "__main__":
+    best_val_loss = float("inf")
+    
     for epoch in range(num_epochs):
         running_loss = 0.0
         model.train()
@@ -126,8 +135,18 @@ if __name__ == "__main__":
 
             print(f'Epoch {epoch + 1}/{num_epochs}, Validation Loss (Cityscapes): {val_loss_cityscapes}, Validation Loss (ADE20K): {val_loss_ade20k}')
 
+            # Save checkpoint if validation loss has improved
+            if val_loss_ade20k < best_val_loss:
+                print(f"Validation loss improved from {best_val_loss:.4f} to {val_loss_ade20k:.4f}. Saving checkpoint.")
+                save_checkpoint(epoch + 1, model, optimizer, val_loss_ade20k, filename=f"checkpoint_best.pth.tar")
+                best_val_loss = val_loss_ade20k
+
             # Stepping the scheduler with the validation loss
             scheduler.step(val_loss_ade20k)
+
+        # Save periodic checkpoint
+        if (epoch + 1) % 5 == 0:
+            save_checkpoint(epoch + 1, model, optimizer, avg_loss, filename=f"checkpoint_epoch_{epoch + 1}.pth.tar")
 
     torch.save(model.state_dict(), 'sky_segmentation_model_25_epochs.pth')
     writer.close()
